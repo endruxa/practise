81ce75b75d1aab999231a1f98261a889e8b22187
Andrey <drunhouse2mail.ru>
2017-12-24 19:25:46 +0300

    table_categories

--- routes/web.php
+++ routes/web.php
@@ -24,6 +24,6 @@ Route::get('/home', 'HomeController@index')->name('home');
  * Adminka
  */
 Route::group(['prefix' => 'admin', 'namespace' => 'Admin', 'middleware' => ['auth']], function (){
-
     Route::get('/', 'DashboardController@dashboard')->name('admin.index');
+    Route::resource('/category', 'CategoryController', ['as' => 'admin']);
 } );
