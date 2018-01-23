a7f6ba59bc9af0ff9e02d940808dbd6f5103fb61
Nesterov Andrey <32617610+endruxa@users.noreply.github.com>
2018-01-23 10:36:22 +0300

    Update app_admin.blade.php

--- resources/views/admin/layouts/app_admin.blade.php
+++ resources/views/admin/layouts/app_admin.blade.php
@@ -9,7 +9,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>@yield('title')Blog</title>
+    <title>@yield('title', config('app.name'))</title>
 
     <!-- Styles -->
     <link href="{{asset('css/clean-blog.min.css')}}" rel="stylesheet">
